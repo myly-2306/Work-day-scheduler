07230fb1944f55fb87d2ee2de38998ae94db52b4
doublerainbow <ly.thuongmy71@gmail.com>
2021-07-18 17:44:14 -0700

    test local storage

--- script.js
+++ script.js
@@ -4,22 +4,12 @@
 // console.log(timeDisplayEl);
 
 // timeDisplayEl.text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+var currentTime = $('#currentDay');
 
-// 
 function displayTime() {
- $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    currentTime.text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 };
 setInterval(displayTime, 1000);
-// function displayTime() {
-//     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-//     now.innerHTML = rightNow;
-// };
-
-// function displayTime() {
-//     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-//     timeDisplayEl.text(rightNow);
-// }
-// setInterval(displayTime, 1000);
 
 // set time range to an array to interate later
 var timeRange = [8, 9, 10, 11, 12, 1, 2, 3, 4, 5];
@@ -29,13 +19,75 @@ var timeRange = [8, 9, 10, 11, 12, 1, 2, 3, 4, 5];
 // if past, grey
 // if present, green
 //  if future, yellow
-// for (i = 0, i < timeRange.length, i++ ) {
 
+// current time in hour format
+// function currentHour() {
+//     var now = moment().hour();
+
+
+
+//     for (i = 0; i < timeRange.length; i++) {
+//     if (i == currentTime)
+//     }
 // }
 
 
-
-
+// var event8 = document.querySelector('#am');
+// event8.textContent = eventAt8;
 // event listerner to each time slot to save event to local storage
 // Identify key and value
 // JSON to stringtify aND parse ;
+
+
+// btn8.addEventListener("click", function() {
+//     localStorage.setItem("event8", eventAt8);
+// }); 
+
+// localStorage.getItem("event8");
+
+var event8 = document.querySelector("#8-am");
+var btn8 = document.querySelector("#btn-8");
+
+function saveEvent() {
+    var allEvent = {
+        event8: event8.value.trim(),
+        
+    };
+    localStorage.setItem("allEvent", JSON.stringify(allEvent));
+}
+
+function renderEvent() {
+    // Use JSON.parse() to convert text to JavaScript object
+    var lastEvent = JSON.parse(localStorage.getItem("allEvent"))
+}
+
+btn8.addEventListener("click", function(event) {
+    event.preventDefault();
+    saveEvent();
+    renderEvent();
+    });
+
+
+
+
+
+
+
+// This function is being called below and will run when the page loads.
+function init() {
+    // Get stored todos from localStorage
+    var storedTodos = JSON.parse(localStorage.getItem("todos"));
+  
+    // If todos were retrieved from localStorage, update the todos array to it
+    if (storedTodos !== null) {
+      todos = storedTodos;
+    }
+  
+    // This is a helper function that will render todos to the DOM
+    renderTodos();
+  }
+  
+  function storeTodos() {
+    // Stringify and set key in localStorage to todos array
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
